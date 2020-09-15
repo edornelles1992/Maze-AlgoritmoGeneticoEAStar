4aba9b13ccf6c763fec2a72c789706888f50d46c
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-09-15 02:42:53 -0300

    criados algoritmo de população inicial e aptidao

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -8,14 +8,23 @@ public class Labirinto {
 
 	static int[] E = { 0, 0 };
 	static int[] S = { 11, 11 };
+	static int geracoes = 20;
 	static int numMovimentos = 35;
+	static int melhorValor = 24;
+	static int piorValor = 0;
+	static int parede = 1;
+	static int tamanho = 12;
 
 	public static void main(String[] args) {
 
 		int[][] labirinto = montarLabirinto();
-		printLabirinto(labirinto);
-		Movimento[] movimentos = geraPopulacaoInicial();
-		System.out.println(Arrays.toString(movimentos));
+		Movimento[][] populacao = new Movimento[numMovimentos][numMovimentos]; 
+		int[] aptidoes = new int[populacao.length];
+		Movimento[][] populacaoIntermediaria = new Movimento[numMovimentos][numMovimentos];
+
+		geraPopulacaoInicial(populacao);
+		atribuiAptidao(populacao, labirinto, aptidoes);
+
 	}
 
 	public static int[][] montarLabirinto() {
@@ -37,6 +46,7 @@ public class Labirinto {
 			System.out.println("Arquivo Não Encontrado!");
 			return null;
 		}
+		printLabirinto(labirinto);
 		return labirinto;
 	}
 
@@ -50,14 +60,72 @@ public class Labirinto {
 			System.out.println();
 		}
 	}
-
-	public static Movimento[] geraPopulacaoInicial() {
-		Random rng = new Random();
-		Movimento[] movimentos = new Movimento[numMovimentos];
-		for (int i = 0; i < movimentos.length; i++) {
-			int randomNum = rng.nextInt((4 - 1) + 1) + 1;
-			movimentos[i] = Movimento.getMovimentoByValue(randomNum); 
+	
+	public static void printPopulacao(Movimento[][] populacao, int[] aptidoes) {
+		System.out.println("População gerada:");
+		for (int i = 0; i < populacao.length; i++) {
+//		      System.out.print(i + " - ");
+			System.out.println(i + " " + Arrays.toString(populacao[i]) + " Aptidão = " + aptidoes[i]);
+//			for (int j = 0; j < populacao[0].length; j++) {
+//				System.out.print(populacao[i][j] + " ");
+//			}
 		}
-		return movimentos;
+	}
+
+	public static Movimento[][] geraPopulacaoInicial(Movimento[][] populacao) {
+		System.out.println();
+		Random rng = new Random();
+		for (int i = 0; i < populacao.length; i++) {
+			for (int j = 0; j < populacao.length; j++) {
+				int randomNum = rng.nextInt((4 - 1) + 1) + 1;
+				populacao[i][j] = Movimento.getMovimentoByValue(randomNum);
+			}
+		}
+		return populacao;
+	}
+
+	public static void atribuiAptidao(Movimento[][] populacao, int[][] labirinto, int[] aptidoes) {
+		int[] posicaoAtual = { 0, 0 };// inicio labirinto
+		for (int i = 0; i < populacao.length; i++) {
+			for (int j = 0; j < populacao.length; j++) {
+				realizaMovimento(posicaoAtual, populacao[i][j], labirinto);			
+			}
+			aptidoes[i] = posicaoAtual[0] + posicaoAtual[1];
+			posicaoAtual[0] = 0;
+			posicaoAtual[1] = 0;
+		}
+		
+		
+		printPopulacao(populacao, aptidoes);		
+	}
+
+	private static void realizaMovimento(int[] posicaoAtual, Movimento movimento, int[][] labirinto) {
+		switch (movimento) {
+		case C: {
+			if (posicaoAtual[0] - 1 >= 0 && labirinto[posicaoAtual[0] - 1][posicaoAtual[1]] != parede) {
+				posicaoAtual[0] = posicaoAtual[0] - 1;
+			}
+		}
+			break;
+		case B: {
+			if (posicaoAtual[0] + 1 < tamanho && labirinto[posicaoAtual[0] + 1][posicaoAtual[1]] != parede) {
+				posicaoAtual[0] = posicaoAtual[0] + 1;
+			}
+		}
+			break;
+		case E: {
+			if (posicaoAtual[1] - 1 >= 0 && labirinto[posicaoAtual[0]][posicaoAtual[1] - 1] != parede) {
+				posicaoAtual[1] = posicaoAtual[1] - 1;
+			}
+		}
+			break;
+		case D: {
+			if (posicaoAtual[1] + 1 < tamanho && labirinto[posicaoAtual[0]][posicaoAtual[1] + 1] != parede) {
+				posicaoAtual[1] = posicaoAtual[1] + 1;
+			}
+		}
+			break;
+		default:
+		}
 	}
 }
